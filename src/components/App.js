f419ea9ca94489690ded579ec72f8672263ee8a8
sachinrajbher15 <sachinrajbher1505@gmail.com>
2024-12-01 00:47:17 +0530

    Updated Files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [task, setTask] = useState("");
+  const [todoTasks, setTodoTask] = useState([]);
+
+  const handleChange = (e) =>{
+    setTask(e.target.value);
+  }
+  const handleClick = () => {
+    if(task.trim() != ""){
+      setTodoTask([...todoTasks, task]);
+      setTask("");
+    }
+  }
+  const handleDelete = (index) => {
+    const updatedTask = todoTasks.filter((_,i) => i!== index);
+    setTodoTask(updatedTask);
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="container">
+      <div className="header">
+        <h2>To-Do List</h2>
+      </div>
+      <div className="add-task-section">
+        <input id="input-field" type="text" onChange={handleChange} value={task}></input>
+        <button id="add-btn" onClick={handleClick}>Add Todo</button>
+      </div>
+
+      <div className="tasks-container">
+        <ul>
+          {todoTasks.map((currentTask, index) => (
+            <li key={index}>
+              {currentTask}
+              <button id="delete-btn" onClick={() => handleDelete(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
